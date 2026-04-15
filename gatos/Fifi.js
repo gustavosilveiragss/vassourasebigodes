cdc896390fa6e1571007a0d872c7f8858d000e55
gustavosilveiragss <gustavo24gss@gmail.com>
2026-04-15 12:37:50 -0300

    reescreve comportamento da Fifi

--- gatos/Fifi.js
+++ gatos/Fifi.js
@@ -1,18 +1,39 @@
 class Fifi extends Gato {
   constructor(x, y) {
     super(x, y, RAIOS.gato, CORES.fifi, 'Fifi')
-    this.dir = p5.Vector.random2D()
-    this.prox = floor(random(60, 120))
+    this.angOffset = random(-PI / 4, PI / 4)
     this.frames = 0
+    this.movendo = false
+    this.duracaoAtual = 120
   }
 
   mover() {
     this.frames++
-    if (this.frames >= this.prox) {
-      this.dir = p5.Vector.random2D()
-      this.prox = floor(random(60, 120))
+
+    let distMouse = dist(mx, my, this.pos.x, this.pos.y)
+
+    if (this.frames >= this.duracaoAtual) {
+      this.movendo = !this.movendo
       this.frames = 0
+      if (this.movendo) {
+        this.angOffset = random(-PI / 4, PI / 4)
+        this.duracaoAtual = floor(random(50, 70))
+      } else {
+        this.duracaoAtual = floor(random(100, 140))
+      }
     }
-    this.vel.add(p5.Vector.mult(this.dir, VEL.normal))
+
+    if (!this.movendo && distMouse < 130) {
+      this.movendo = true
+      this.frames = 0
+      this.angOffset = random(-PI / 4, PI / 4)
+      this.duracaoAtual = floor(random(50, 70))
+    }
+
+    if (this.movendo) {
+      let ang = atan2(my - this.pos.y, mx - this.pos.x) + this.angOffset
+      let dir = createVector(cos(ang), sin(ang))
+      this.vel.add(p5.Vector.mult(dir, VEL.normal))
+    }
   }
 }
